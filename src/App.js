0035932cdfdaab22e2e19d958d860555a0dcf0b9
Adrien Soulié <52654295+adriensoulie@users.noreply.github.com>
2020-04-18 23:30:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+import Formulaire from './components/Formulaire'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='box' >
+        <div>
+          <div className="messages">
+            Messages
+          </div>
+        </div>
+        <Formulaire />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const Error = () => (
+    <h2 className='Error'> Nothing is there !</h2>
+)
+
+export default Error
