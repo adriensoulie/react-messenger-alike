668d9293469267d27512317a4866ce1a7452a363
Adrien Soulié <52654295+adriensoulie@users.noreply.github.com>
2020-04-19 20:43:45 +0700

    state update for messenge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
 import './App.css'
 import Formulaire from './components/Formulaire'
+import Message from './components/Message'
 
 class App extends Component {
+
+  state = {
+      messages: {},
+      pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = { ...this.state.messages }
+    messages[`message-${Date.now()}`] = message 
+    this.setState({ messages })
+  }
+
   render () {
     return (
       <div className='box' >
         <div>
           <div className="messages">
-            Messages
+            <Message/>
+            <Message/>
+            <Message/>
           </div>
         </div>
-        <Formulaire />
+        <Formulaire 
+        length={140} 
+        pseudo={this.state.pseudo}
+        addMessage={this.addMessage} />
       </div>
     )
   }
---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Message = () => {
+    return (
+        <div>
+            <p className='user-message'>
+                My message 
+            </p>
+        </div>
+    )
+}
+
+export default Message 
